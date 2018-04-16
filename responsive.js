8b938b3ca50fd8dd0783e2ad33c5dc3c0f514c3b
Justin Farrow <landsurveyorsunited@gmail.com>
2018-04-16 17:49:27 -0400

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -1,4 +1,4 @@
-var defaultURL = 'mattkersley.com'; //<---- CHANGE TO YOUR WEBSITE URL
+var defaultURL = 'jbirds.co'; //<---- CHANGE TO YOUR WEBSITE URL
 
 //show loading graphic
 function showLoader(id) {
